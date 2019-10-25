544ae7981c63889343aaa8392f364da3f7e6785a
王修斌 <iosgg@outlook.com>
2019-10-25 23:29:48 +0800

    imp get method

--- httpd.c
+++ httpd.c
@@ -6,13 +6,18 @@
 #include <stdlib.h>
 #include <string.h> // for memset
 #include <ctype.h>
+#include <sys/stat.h>
+
+#define SERVER_STRING "Server: TinyHttpd/0.1.0\r\n"
+
+char *document = ".";
 
 void error_die(const char *e) {
     perror(e);
     exit(1);
 }
 
-int get_line(int fd, char *buff, int size, int index) {
+int get_line(int fd, char *buf, int size, int index) {
     int i = index;
     char c = '\0';
     ssize_t n;
@@ -22,34 +27,34 @@ int get_line(int fd, char *buff, int size, int index) {
             c = '\n';
             break;
         }
-        buff[i] = c;
+        buf[i] = c;
         i++;
     }
-    buff[i] = '\0';
+    buf[i] = '\0';
     return i;
 }
 
 // 判断是否是 header 结尾
-int is_end(char *buff, int size) {
+int is_end(char *buf, int size) {
     if (size > 4 &&
-        buff[size] == '\0' &&
-        buff[size - 1] == '\n' &&
-        buff[size - 2] == '\r' &&
-        buff[size - 3] == '\n' &&
-        buff[size - 4] == '\r') {
+        buf[size] == '\0' &&
+        buf[size - 1] == '\n' &&
+        buf[size - 2] == '\r' &&
+        buf[size - 3] == '\n' &&
+        buf[size - 4] == '\r') {
         return 1;
     }
     return 0;
 }
 
-int parse_url(char *buff, int idx, char *url, char *path, char *query) {
+int parse_url(char *buf, int idx, char *url, char *path, char *query) {
 
     int i = 0;
     char matchPath = '1';
     int path_len = 0;
     int query_len = 0;
     do {
-        char c = buff[idx];
+        char c = buf[idx];
         url[i] = c;
 
         if (c == '?') {
@@ -62,45 +67,142 @@ int parse_url(char *buff, int idx, char *url, char *path, char *query) {
         } else {
             query[query_len++] = c;
         }
-    } while (!isspace(buff[idx]) && ++idx && ++i);
+    } while (!isspace(buf[idx]) && ++idx && ++i);
     url[i] = '\0';
-    path[path_len] = '\0';
-    query[query_len - 1] = '\0';
+    path[path_len - 1] = '\0';
+    if (query_len > 0) {
+        query[query_len - 1] = '\0';
+    } else {
+        query[0] = '\0';
+    }
     return idx;
 }
 
-int parse_method(char *buff, int idx, char *method) {
+int parse_method(char *buf, int idx, char *method) {
     int i = 0;
     do {
-        method[i] = buff[idx];
-    } while (!isspace(buff[idx]) && ++idx && ++i);
+        method[i] = buf[idx];
+    } while (!isspace(buf[idx]) && ++idx && ++i);
     method[i] = '\0';
     return idx;
+}
+
+void not_found(int fd) {
+
+    char buf[1024];
+
+    sprintf(buf, "HTTP/1.0 404 NOT FOUND\r\n");
+    sprintf(buf, "%s%s", buf, SERVER_STRING);
+    sprintf(buf, "%sContent-Type: text/html\r\n", buf);
+    sprintf(buf, "%s\r\n", buf);
+    sprintf(buf, "%s<HTML><TITLE>Not Found</TITLE>\r\n", buf);
+    sprintf(buf, "%s<BODY><P>404\r\n", buf);
+    sprintf(buf, "%sSorry, Page Not Found :(\r\n", buf);
+    sprintf(buf, "%s</BODY></HTML>\r\n", buf);
+    send(fd, buf, strlen(buf), 0);
+}
+
+void headers(int fd, char *file_type, long file_size) {
+
+    char buf[1024];
+
+    strcpy(buf, "HTTP/1.0 200 ok\r\n");
+
+    sprintf(buf, "%s%s", buf, SERVER_STRING);
+    sprintf(buf, "%sContent-type: %s\r\n", buf, file_type);
+//    sprintf(buf, "%sContent-length: %ld\r\n", buf, file_size);
+
+    sprintf(buf, "%s\r\n", buf);
+    send(fd, buf, strlen(buf), 0);
+}
+
+void serve_file(int fd, char *path) {
+    FILE *file = fopen(path, "r");
+
+    if (file == NULL) {
+        not_found(fd);
+    } else {
+
+        fseek(file, 0L, SEEK_END);
+        long size = ftell(file);
+
+        headers(fd, "text/html", size);
+
+        char buf[1024];
+        do {
+            fgets(buf, sizeof(buf), file);
+            send(fd, buf, sizeof(buf), 0);
+        } while (!feof(file));
+    }
+    fclose(file);
+}
+
+void execute_get(int fd, char *url, char *p, char *query) {
+    int cgi = 0;
+    if (query[0] != '\0')
+        cgi = 1;
+
+    char path[2048];
+    sprintf(path, "%s%s", document, p);
+
+    if (path[strlen(path) - 1] == '/')
+        strcat(path, "index.html");
+
+    struct stat st;
+
+    if (stat(path, &st) == -1)
+        return not_found(fd);
+
+    if ((st.st_mode & S_IFMT) == S_IFDIR)   // 是目录就补上默认文件
+        strcat(path, "/index.html");
+
+    // 判断是否是可执行文件
+    if ((st.st_mode & S_IXUSR) || (st.st_mode & S_IXGRP) || (st.st_mode & S_IXOTH)) {
+        cgi = 1;
+    }
+
+    if (cgi == 1) {
+        // todo: execute cgi
+    } else {
+        serve_file(fd, path);
+    }
+}
+
+void execute_post(int fd, char *url, char *path) {
+
 }
 
 void accept_request(void *args) {
 
     int fd = (int)args;
 
-    char buff[1024];
+    char buf[1024];
     char method[255];
-    char url[255];
-    char path[255];
+    char url[1024];
+    char path[1024];
     char query[255];
 
     int line = 0;
     int index = 0;
 
-    while (is_end(buff, index) == 0) {
-        index = get_line(fd, buff, sizeof(buff), index);
+    while (is_end(buf, index) == 0) {
+        index = get_line(fd, buf, sizeof(buf), index);
         line++;
     }
 
     int idx = 0;
-    idx = parse_method(buff, idx, method);
-    idx = parse_url(buff, ++idx, url, path, query);
+    idx = parse_method(buf, idx, method);
+    idx = parse_url(buf, ++idx, url, path, query);
 
-    printf("%s", buff);
+    if (strcasecmp(method, "GET") == 0) {
+        execute_get(fd, url, path, query);
+    } else if (strcasecmp(method, "POST") == 0) {
+        // todo: execute post
+    } else {
+        // unimplemented method
+    }
+
+//    printf("%s", buf);
 
     close(fd);
 }
